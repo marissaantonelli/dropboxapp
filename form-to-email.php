010581eaab8942f50ebc95d3ff09b77aa9c637b0
Marissa Antonelli <marissa.antonelli@outlook.com>
2018-07-24 21:24:36 -0400

    first commit testing

---
+++ form-to-email.php
@@ -0,0 +1,16 @@
+<? php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'yourname@yourwebsite.com';
+$email_subject = "New Form Submission";
+$email_body = "You have received a new message from Leon.\n"
+"Here is the message: \n $message";
+
+$to = "yourname@yourwebsite.com";
+$headers = "From: $email_from \r\n";
+$headers . = "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+?>
